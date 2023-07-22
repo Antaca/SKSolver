b0078b62d4ed807eec779661037a59a619759448
Antaca <antoniocarreterodiaz@gmail.com>
2023-07-22 20:03:39 +0200

    Added some config files

--- src/main/java/es/lonely/sksolver/App.java
+++ src/main/java/es/lonely/sksolver/App.java
@@ -1,17 +1,28 @@
 package es.lonely.sksolver;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Hello world!
  */
-public final class App {
-    private App() {
+public final class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        final Integer width = 640;
+        final Integer height = 480;
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), width, height);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        launch();
     }
 }
